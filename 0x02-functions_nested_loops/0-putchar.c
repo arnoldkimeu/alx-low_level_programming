517560161facadfaf814855ed85e3c5d0dd6709a
arnoldkimeu <arnoldkimeu@gmail.com>
2022-09-14 08:14:26 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,3 @@
-/**
- * File: 0-putchar.c
- * Auth: Arnold Kimeu
- */
-
 #include "main.h"
 
 /**
@@ -10,17 +5,18 @@
  *
  * Return: 0
  */
+
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
-return (0);
+return: 0
 }
